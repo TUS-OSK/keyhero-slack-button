fcdcb52393cc864beccb5d90a9580ed4aca3e2a3
minddrop <ryooooooo1@gmail.com>
2019-05-10 13:19:57 +0900

    保存されている設定をポップアップに反映

--- src/options.js
+++ src/options.js
@@ -1,16 +1,29 @@
-'use strict'
+const endpoint = document.getElementById('url')
+const hoge = document.getElementById('hoge')
+const auto = document.getElementById('auto')
+chrome.storage.local.get({ endpoint }, r => {
+  endpoint.value = r.endpoint
+})
+chrome.storage.local.get({ hoge }, r => {
+  hoge.value = r.hoge
+})
+chrome.storage.local.get({ auto }, r => {
+  auto.checked = r.auto
+})
 
-const storage = chrome.storage
 function saveOptions() {
-  const serverUrl = document.getElementById('url')
-  const hoge = document.getElementById('hoge')
-  storage.sync.set({ serverUrl: serverUrl, hoge: hoge }, () => {
-    const status = document.getElementById('status')
-    status.textContent = 'Settings saved.'
-    setTimeout(() => {
-      status.textContent = ''
-    }, 800)
-  })
+  chrome.storage.local.set(
+    { endpoint: endpoint.value, hoge: hoge.value, auto: auto.checked },
+    () => {
+      const status = document.getElementById('status')
+      status.textContent = 'Settings saved.'
+      setTimeout(() => {
+        status.textContent = ''
+      }, 800)
+    }
+  )
 }
 
-document.getElementById('save').addEventListener('click', saveOptions)
+document.getElementById('save').addEventListener('click', () => {
+  saveOptions()
+})
